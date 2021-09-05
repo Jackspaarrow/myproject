91f0725a3aaeb38335df17769d5beb43295f0b1e
JackSpaarrow <krishmani705@gmail.com>
2021-09-05 12:01:30 +0530

    ascending order three numbers

---
+++ src/character/value/SwapTwo.java
@@ -0,0 +1,71 @@
+package character.value;
+
+public class SwapTwo {
+
+	public static void main(String[] args) {
+		int a = 3,b = 1,c = 2 ,d;
+		if((a<b)&&(b>c))
+		{
+			d = b; //d= 3;
+			b = c; //b = 1;
+			c = d; //c = 3
+					//a =2	
+			if((a<b)&&(b<c))
+			{
+				System.out.println(a);
+				System.out.println(b);
+				System.out.println(c);
+			}
+			else
+			{
+					d = b; // d = 2
+					b = a;	//a = 1
+					a = d;  //b = 2
+				
+					System.out.println(a);
+					System.out.println(b);
+					System.out.println(c);
+			}
+		}else if((a>b)&&(b<c))
+		{
+			d = b;
+			b = a;
+			a = d;
+		
+			if((a<b)&&(b<c))
+			{
+				System.out.println(a);
+				System.out.println(b);
+				System.out.println(c);
+			}
+			else
+			{
+				d = b;
+				b = c;
+				c = d;
+			
+				System.out.println(a);
+				System.out.println(b);
+				System.out.println(c);
+			}
+			
+		}else if((a>b)&&(b>c))
+		{
+			d = a;
+			a = c;
+			c = d;
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+		}
+		else
+		{
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			
+		}
+
+	}
+
+}
